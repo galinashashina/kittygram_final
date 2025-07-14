9c2d8ee3566d8f09d8877622dc1a76b570119c27
Галина Шашина <g.sshashina@yandex.ru>
2025-07-14 17:19:59 +0300

    fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-# ALLOWED_HOSTS = ['kittygram999.zapto.org', '127.0.0.1', 'localhost', 'taski999.zapto.org']
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
